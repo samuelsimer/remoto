bf9164f0d004c3d5637ecc22f0725889c6cda7eb
samuelremja <samuelremja@gmail.com>
2018-10-03 13:39:47 +0200

    ahora muestra tambien la ciudad en la que nací

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba{
 	public static void main (String args[]){
-	 System.out.println("Samuel Remis Jañez");
+	 System.out.println("Samuel Remis Jañez\nLeón \nTrobajo del Camino");
 	}
 }
